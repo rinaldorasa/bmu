dde9b1ceacf1eb58a9aee5f4c14be915d3d34f91
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2020-08-13 11:00:24 +0200

    Add files via upload

---
+++ etichette-circolari.js
@@ -0,0 +1,264 @@
+/**************************
+|
+| etichette-circolari.js
+|
+| 202008121645
+|
+**************************/
+
+$('#NEMO1992').click(function() {
+    	
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO1993').click(function() {
+    	
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO1994').click(function() {
+	
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO1995').click(function() {
+	
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO1996').click(function() {
+	
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO1997').click(function() {
+	
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO1998').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO1999').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO2000').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+$('#NEMO2001').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2002').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2003').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2004').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2005').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2006').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2007').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2008').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2009').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+$('#NEMO2010').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+    $('#ALTER_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+});
+
+
+
+
+
+
+
+
+$('#colore1992').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+$('#colore1993').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+$('#colore1994').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+$('#colore1995').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+$('#colore1996').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+$('#colore1997').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+$('#colore1998').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+$('#colore1999').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2000').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2001').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2002').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2003').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2004').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2005').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2006').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2007').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2008').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2009').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
+$('#colore2010').click(function() {
+
+    $('#AGENDA_TITOLO').removeClass('grey empty circular label').addClass('green empty circular label');	
+    $('#ALTER_TITOLO').removeClass('green empty circular label').addClass('grey empty circular label');	
+});
+
+
