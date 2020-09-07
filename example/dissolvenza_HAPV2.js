bba56efa0177094c15d02b1e8da8f6e0a3dd8484
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2020-09-07 13:02:26 +0200

    Add files via upload

---
+++ example/dissolvenza_HAPV2.js
@@ -0,0 +1,27 @@
+document.getElementById("dissolvenza_HAPV2").innerHTML=
+//
+' <style> '+
+//
+//
+// 
+// effetto fade
+//
+' .DISSOLVENZA {'+
+'  animation: fadeIn ease 2.5s;'+
+'  -webkit-animation: fadeIn ease 2.5s;'+
+'  -moz-animation: fadeIn ease 2.5s;'+
+'  -o-animation: fadeIn ease 2.5s;'+
+'  -ms-animation: fadeIn ease 2.5s;'+
+'  }'+
+'  @keyframes fadeIn {'+
+'  0% {opacity:0;}'+
+'  100% {opacity:1;}'+
+'  }'+
+// 
+//
+//
+//
+' </style> '+
+''+
+''
+;
---
+++ example/progress_HAPV2.js
@@ -0,0 +1,30 @@
+document.getElementById("progress_HAPV2").innerHTML=
+//
+' <style> '+
+//
+//
+// 
+// indicatore progresso
+' .capo_pagina_progress{ position:fixed; } '+
+' .capo_pagina_progress{ top:0rem; } '+
+' .capo_pagina_progress{ left:0rem; } '+
+' .capo_pagina_progress{ width:100%; } '+
+' .capo_pagina_progress{ height:'+CAPO_PROGRESS_ALTEZZA+'rem;} '+
+' .capo_pagina_progress{ background-color: burlywood !important; } '+
+' .capo_pagina_progress{ z-index:100; } '+
+' .capo_pagina_progress{ } '+
+' .capo_pagina_progress{ } '+
+' '+
+' '+
+' .indicatore_progress{ width:0rem; }'+
+' .indicatore_progress{ height: 0.5em; }'+
+' .indicatore_progress{ background-color: maroon !important; }'+
+' .indicatore_progress{}'+
+' '+
+' '+
+//
+//
+' </style> '+
+''+
+''
+;
