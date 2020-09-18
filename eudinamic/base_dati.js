e3be4c48c640f32e9147fa90fc3f148dce6d3f1d
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2020-09-18 12:47:27 +0200

    Add files via upload

---
+++ eudinamic/base_dati.js
@@ -0,0 +1,53 @@
+/////////////////////////////
+//
+// bsn_dati 20200910-1000
+//
+/////////////////////////////
+
+  //
+  //
+    var numero=Math.random();
+  // 
+    numero=numero*10000;
+    numero=Math.floor(numero);
+    aggiunto=numero.toString();
+    esteso="?";
+    esteso=esteso+aggiunto;
+
+    var bsn_array_url=new Array();
+    bsn_array_url[0]='BeatSuperNova';
+    bsn_array_url[1]='http://beatsupernovarasa.altervista.org/beatpics/bp000027.html'+esteso;
+    bsn_array_url[2]='http://beatsupernovarasa.altervista.org/beatpics/bp000074.html'+esteso;
+    bsn_array_url[3]='http://beatsupernovarasa.altervista.org/beatpics/bp000139.html'+esteso;
+    bsn_array_url[4]='http://beatsupernovarasa.altervista.org/beatpics/bp000151.html'+esteso;
+    bsn_array_url[5]='http://beatsupernovarasa.altervista.org/beatpics/bp000186.html'+esteso;
+    bsn_array_url[6]='';
+    bsn_array_url[7]='';
+    bsn_array_url[8]='';
+    
+    var bsn_beatpics_url=new Array();
+    bsn_beatpics_url[0]='BeatSuperNova';
+    bsn_beatpics_url[1]='http://beatsupernovarasa.altervista.org/beatpics/bp000027.html'+esteso;
+    bsn_beatpics_url[2]='http://beatsupernovarasa.altervista.org/beatpics/bp000074.html'+esteso;
+    bsn_beatpics_url[3]='http://beatsupernovarasa.altervista.org/beatpics/bp000139.html'+esteso;
+    bsn_beatpics_url[4]='http://beatsupernovarasa.altervista.org/beatpics/bp000151.html'+esteso;
+    bsn_beatpics_url[5]='http://beatsupernovarasa.altervista.org/beatpics/bp000186.html'+esteso;
+    bsn_beatpics_url[6]='';
+    bsn_beatpics_url[7]='';
+    bsn_beatpics_url[8]='';
+     
+//
+//
+ var lorem_ipsum = new Array();
+ lorem_ipsum[0] = 'LOREM IPSUM';
+ 
+ lorem_ipsum[1] = 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo, quando un anonimo tipografo prese una cassetta di caratteri e li assemblò per preparare un testo campione. È sopravvissuto non solo a più di cinque secoli, ma anche al passaggio alla videoimpaginazione, pervenendoci sostanzialmente inalterato. Fu reso popolare, negli anni ’60, con la diffusione dei fogli di caratteri trasferibili “Letraset”, che contenevano passaggi del Lorem Ipsum, e più recentemente da software di impaginazione come Aldus PageMaker, che includeva versioni del Lorem Ipsum.'
+ 
+//
+//
+
+var lorem_ipsum_immagine = new Array();
+lorem_ipsum_immagine[0]='http://beatsupernovarasa.altervista.org/immagini/rinaldo_on_lambretta_1954.jpg';
+
+
+
