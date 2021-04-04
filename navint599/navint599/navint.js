e571cf15dffdf556fd02114838aceaae59a9762c
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-04-04 11:39:34 +0200

    Add files via upload

---
+++ navint599/navint599/navint.js
@@ -0,0 +1,34 @@
+
+document.getElementById("navint").innerHTML=
+''+
+''+
+'<section class="m-0 p-0 bg-warning" style="position:fixed;top:0px;left:0px;">'+
+'<p>'+
+'<a style="position:static;top:0;left:0;" href="#">'+
+'<b class="p-0" >'+
+'<i class="bi-chevron-up bg-warning" style="color: maroon;"></i>'+
+'</b>'+
+'</a>'+
+'</p>'+
+''+
+'<p>'+
+'<a style="position:static;top:40px;left:0;" href="#fine">'+
+'<b class="p-0" >'+
+'<i class="bi-chevron-down bg-warning" style="color: maroon;"></i>'+
+'</b>'+
+'</a>'+
+'</p>'+
+''+
+'<p>'+
+'<a style="position:static;top:162px;left:0;" href="http://venexiatre.altervista.org/WORKOU/OGGI/20191119/20191119f_1-CREEK.html">'+
+'<b class="p-0 bg-warning" >'+
+'<i class="bi-house" style="color:oldlace;"> </i>'+
+'</b>'+
+'</a>'+
+''+
+'</p>'+
+'</section>'+
+''+
+''+
+''
+;
